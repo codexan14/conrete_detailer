691e6a60afda1684600d9b17ee20e7ec57203e3f
codex.an14 <global.an14@mac>
2025-06-30 22:41:47 -0400

    Added a Svelte GUI

--- api/main.py
+++ api/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import math 
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En producción: pon solo tu frontend
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Entrada(BaseModel):
+    base: float
+    altura: float
 
 @app.get("/")
 async def root(number: int) -> dict[str, float]:
      return{"number": number**2}
+
+
+@app.post("/producto")
+async def func(entrada: Entrada) -> dict[str, float]:
+     print(2)
+     return {"resultado": entrada.base * entrada.altura}
