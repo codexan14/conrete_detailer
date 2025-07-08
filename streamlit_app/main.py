8dff79ef513a2a6fa7c1a2332d6dc42e811dbc55
codex.an14 <global.an14@mac>
2025-07-07 22:42:54 -0400

    Removed some files

--- streamlit_app/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-if root_path not in sys.path:
-    sys.path.insert(0, root_path)
-
-print("sys.path (after insertion):")
-print(sys.path)
